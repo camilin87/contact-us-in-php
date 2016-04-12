c0425a22f3fb39e30adfb599d7df62a7782ad425
camilin87 <camilin87@gmail.com>
2016-04-12 07:07:50 -0400

    create the contactus controller

---
+++ tests/contactUsControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+require "src/contactUsController.php";
+
+class ContactUsControllerTest extends PHPUnit_Framework_TestCase {
+    public function testControllerExists($value='')
+    {
+        $c = new ContactUsController();
+        $this->assertNotNull($c);
+    }
+}
+?>
