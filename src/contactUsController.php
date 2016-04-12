0795d412647ae39c1f823dc9c2a3d47aef2b7a16
Camilo Sanchez <camilin87@gmail.com>
2016-04-12 12:43:13 -0400

    make the new test pass... wait the old tests now are broke

--- src/contactUsController.php
+++ src/contactUsController.php
@@ -1,12 +1,16 @@
 <?php 
 class ContactUsController{
     private $errorHandler = NULL;
+    private $connectionFactory = NULL;
 
     function __construct($errorHandler, $connectionFactory){
         $this->errorHandler = $errorHandler;
+        $this->connectionFactory = $connectionFactory;
     }
 
     public function processRequest(){
+        $this->connectionFactory->createNew("mysql:host=127.0.0.1;dbname=TddContactUs", "root", "");
+
         if (!isset($_POST["txtName"])){
             $this->errorHandler->displayError("no name provided");
             return;
