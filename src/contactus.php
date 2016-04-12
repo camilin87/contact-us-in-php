83a88578bc00e26af2af4b35ba06c8fd5ce66e6d
Camilo Sanchez <camilin87@gmail.com>
2016-04-12 18:29:17 -0400

    use the new DbConnectionFactory and fix one error... oh shit we forgot something else

--- src/contactus.php
+++ src/contactus.php
@@ -1,6 +1,11 @@
 <?php 
     require "contactUsController.php";
+    require "dbConnectionFactory.php";
 
-    $c = new ContactUsController(NULL, NULL, NULL);
+    $c = new ContactUsController(
+        NULL,
+        new DbConnectionFactory(),
+        NULL
+    );
     $c->processRequest();
 ?>
