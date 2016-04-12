1a08e5cd9586e10f4d873f62dda4517a026aac5c
Camilo Sanchez <camilin87@gmail.com>
2016-04-12 19:20:08 -0400

    actually no, no need to pass the settings in this test

--- tests/contactUsControllerTest.php
+++ tests/contactUsControllerTest.php
@@ -53,13 +53,12 @@ class ContactUsControllerTest extends PHPUnit_Framework_TestCase {
     public function testDisplayAnErrorIfNoEmailIsProvided() {
         $_POST["txtName"] = "john doe";
 
-        $settingsReaderMock = $this->createSettingsReaderMock();
         $errorHandlerMock = $this->createErrorHandlerMock();
         $errorHandlerMock->expects($this->once())
                          ->method('displayError')
                          ->with($this->equalTo('no email provided'));
 
-        $c = new ContactUsController($errorHandlerMock, NULL, NULL, $settingsReaderMock);
+        $c = new ContactUsController($errorHandlerMock, NULL, NULL, NULL);
 
         $c->processRequest();
     }
